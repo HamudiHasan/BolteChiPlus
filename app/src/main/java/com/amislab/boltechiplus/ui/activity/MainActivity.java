ab0f0ffba23aa7c34be749e18f728c99c498e3e1
HamudiHasan <hhsonet@gmail.com>
2018-12-29 02:13:41 +0600

    drawer

--- app/src/main/java/com/amislab/boltechiplus/ui/activity/MainActivity.java
+++ app/src/main/java/com/amislab/boltechiplus/ui/activity/MainActivity.java
@@ -1,11 +1,19 @@
 package com.amislab.boltechiplus.ui.activity;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
 
 import com.amislab.boltechiplus.R;
 import com.amislab.boltechiplus.adapter.CategoryAdapter;
@@ -14,16 +22,24 @@ import com.amislab.boltechiplus.model.Category;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
     private RecyclerView recyclerView;
     private List<Category> categoryList = new ArrayList<>();
     private CategoryAdapter adapter;
-
+    private Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        toolbar = findViewById(R.id.toolbar_main);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        // set Drawer for others options
+        setDrawer();
 
         adapter = new CategoryAdapter(this, categoryList);
         recyclerView = findViewById(R.id.rv_main);
@@ -37,5 +53,35 @@ public class MainActivity extends AppCompatActivity {
         categoryList.add(new Category());
         categoryList.add(new Category());
         adapter.notifyDataSetChanged();
+    }
+
+    /**
+     * Set navigation Drawer
+     */
+    private void setDrawer() {
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        View headerView = navigationView.getHeaderView(0);
+
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+
+      /*  if (id == R.id.nav_book_a_space) {
+            // AppUtil.Toast("Book A Parking Space", this);
+        } */
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
